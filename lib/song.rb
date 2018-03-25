87418eefbcc1a3917c021cff0cacacf0c8e83f5e
Kayla Cross <kac288@georgetown.edu>
2018-03-25 20:07:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,6 +36,7 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if find_by_name(song_name)
+    create_by_name(song_name) unless find_by_name(song_name)
+    find_by_name(song_name)
   end
 end
