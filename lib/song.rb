6bc742c24d3743f79ea160a75acb44e989158ee0
Kayla Cross <kac288@georgetown.edu>
2018-03-25 20:18:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,9 +45,9 @@ class Song
   end
 
   def self.new_from_filename(file_name)
-    song_artist, song_name = file_name.chomp('.mp3').split('-').chomp
-    song = find_or_create_by_name(song_name)
-    song.artist_name = song_artist
+    song_artist, song_name = file_name.chomp('.mp3').split('-')
+    song = find_or_create_by_name(song_name.chomp)
+    song.artist_name.chomp = song_artist
     song
   end
 end
