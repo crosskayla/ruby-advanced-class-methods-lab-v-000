84da2b46365a50d14d6ff918ce62187db934dfab
Kayla Cross <kac288@georgetown.edu>
2018-03-25 19:14:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +11,12 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create(name)
+    self.New
+    self.class.all << name
+  end
 
 end
+
+song1 = Song.new
